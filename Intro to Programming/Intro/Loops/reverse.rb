4b2bc94f554646d0d05fb0968f1cb19eb9aa4535
Miguel Ben <itsmigben@gmail.com>
2019-12-08 14:29:04 -0500

    completed reverse.rb

---
+++ Intro to Programming/Intro/Loops/reverse.rb
@@ -0,0 +1,20 @@
+=begin
+  Instructions:
+  Write a method reverse(word) that takes in a string word and returns the word with its letters in reverse order.
+-end
+
+
+
+def reverse(word)
+  i = word.size
+  rev = ""
+  while i > 0
+    rev+= word[i - 1]
+    i-= 1
+  end
+  rev
+end
+
+puts reverse("cat")          # => "tac"
+puts reverse("programming")  # => "gnimmargorp"
+puts reverse("bootcamp")     # => "pmactoob"
