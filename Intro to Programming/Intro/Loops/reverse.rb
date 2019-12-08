6854991276a4b41463c3c086b35be389373d32cd
Miguel Ben <itsmigben@gmail.com>
2019-12-08 14:44:57 -0500

    Update reverse.rb

--- Intro to Programming/Intro/Loops/reverse.rb
+++ Intro to Programming/Intro/Loops/reverse.rb
@@ -2,20 +2,6 @@
   Instructions:
   Write a method reverse(word) that takes in a string word and returns the word with its letters in reverse order.
 
-  reversed = ""
-  
-  i = 0
-  while i < word.size
-    reversed = word[i] + reversed
-    i += 1
-  end
-  
-  if word == reversed
-    return true
-  else
-    return false
-  end
-
 -end
 
 
