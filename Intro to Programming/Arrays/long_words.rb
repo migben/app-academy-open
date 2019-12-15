eb7be83362034fd7a5a61c0b6c184c4a81474456
Miguel Ben <itsmigben@gmail.com>
2019-12-15 15:40:18 -0500

    Create long_words.rb

---
+++ Intro to Programming/Arrays/long_words.rb
@@ -0,0 +1,14 @@
+=begin
+Write a method select_long_words(words) that takes in an array 
+of words and returns a new array containing all of the words of 
+the original array that are longer than 4 characters.
+
+=end
+
+def select_long_words(words)
+	return words.select {|word| word.length > 4}
+end
+
+print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
+puts
+print select_long_words(["keep", "coding"])                               # => ["coding"]
