7e490d51363d0a42a103a97d288ba1d63b9bf3c2
Miguel Ben <itsmigben@gmail.com>
2019-12-18 10:05:42 -0500

    Create toIniitials.rb

---
+++ Intro to Programming/Intro/arrayMethods&Enums/toIniitials.rb
@@ -0,0 +1,16 @@
+def to_initials(name)
+  arr = name.split(" ")
+  
+  initials = ""
+  
+  arr.each {|word|
+  	initials += word[0]
+  }
+  
+  return initials
+
+end
+
+puts to_initials("Kelvin Bridges")      # => "KB"
+puts to_initials("Michaela Yamamoto")   # => "MY"
+puts to_initials("Mary La Grange")      # => "MLG"
